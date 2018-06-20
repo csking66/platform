076b5ebd0264e46d2f4b66c0a3632e6d47d2ff76
cs <3003800762@qq.com>
2018-06-20 17:26:50 +0800

    eureka 集群注册

--- platform/eureka-client/src/main/java/com/eureka/client/controller/ConsumerController.java
+++ platform/eureka-client/src/main/java/com/eureka/client/controller/ConsumerController.java
@@ -9,11 +9,15 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerController {
 
+	/**
+	 * restTemplate 调用，无需添加@EnableDiscoveryClient 或者EnableEurekaClient
+	 * 如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
+	 */
 	@Autowired
     RestTemplate restTemplate;
 	
-	@RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
 	public String helloController() {
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+        return restTemplate.getForEntity("http://eureka-server/hello", String.class).getBody();
     }
 }
