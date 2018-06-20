9d3c23711ca790806e40560114ec7298dee07c10
cs <3003800762@qq.com>
2018-06-20 17:33:21 +0800

    RestTemplate 调用

--- platform/eureka-server/src/main/java/com/eureka/server/ServerApplicant.java
+++ platform/eureka-server/src/main/java/com/eureka/server/ServerApplicant.java
@@ -2,10 +2,8 @@ package com.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ServerApplicant {
 
 	public static void main(String[] args) {
