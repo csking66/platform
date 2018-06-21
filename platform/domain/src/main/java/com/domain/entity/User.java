723ca97205335f3c3258972aaca04529ffb94c37
cs <3003800762@qq.com>
2018-06-21 10:28:23 +0800

    新增Domain

---
+++ platform/domain/src/main/java/com/domain/entity/User.java
@@ -0,0 +1,49 @@
+package com.domain.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+@ApiModel(value = "User ", description = "用户")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 2424944657192343647L;
+	
+	@ApiModelProperty(value = "ID")
+	private Long id;
+	
+	@ApiModelProperty(value = "姓名")
+	private String name;
+	
+	@ApiModelProperty(value = "年龄")
+	private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+
+}
