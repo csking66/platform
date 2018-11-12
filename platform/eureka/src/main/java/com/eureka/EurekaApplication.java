4f814d55013b3f748020376bdc60c855bd1388d2
windows <windows@DESKTOP-6QHE86A>
2018-11-12 14:03:14 +0800

    eureka

---
+++ platform/eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+* @ClassName: EurekaApplication
+* @Description: 
+* @date 2018年11月11日
+*
+*/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
